9db2f4dfa4e5a48f021c61bc35b437e3bdf2fdf4
Sivaprakash Ragavan <mail@sivragav.com>
2017-07-14 17:28:44 -0400

    Added a content editable angular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContentEditableDirective } from './contenteditable-model.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentEditableDirective
   ],
   imports: [
     BrowserModule
